9887055d5503a1c41cf30d980d9449f95d45c8ce
Marcin <marcin.laskowski.07@gmail.com>
2024-04-27 12:41:55 +0200

    Minor cleanup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,11 +30,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
     buildFeatures {
         compose = true
@@ -50,9 +50,6 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.retrofit)
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
